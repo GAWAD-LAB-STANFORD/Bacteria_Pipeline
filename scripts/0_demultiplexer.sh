2fe0042373930956a1d8d43c69fd6745f261e33f
David Klein <kleinjdavid@gmail.com>
2020-11-22 17:03:22 -0800

    copy from WGS WES pipeline

--- scripts/0_demultiplexer.sh
+++ scripts/0_demultiplexer.sh
@@ -6,32 +6,38 @@
 #SBATCH --time=6:00:00
 #SBATCH --partition=cgawad
 
+START_TIME=$(date +%s)
 while [ "$1" != "" ]; do
     case $1 in
-        --run_dir )         shift
-                            RUN_DIR=$1
-                            ;;
-        --sample_sheet )    shift
-                            SAMPLE_SHEET=$1
-                            ;;
-        --fastq_dir )       shift
-                            FASTQ_DIR=$1
-                            ;;
+        --run_dir )             shift
+                                RUN_DIR=$1
+                                ;;
+        --sample_sheet )        shift
+                                SAMPLE_SHEET=$1
+                                ;;
+        --fastq_dir )           shift
+                                FASTQ_DIR=$1
+                                ;;
+        --pipeline_status )     shift
+                                PIPELINE_STATUS=$1
+                                ;;
     esac
     shift
 done
 
-if [ -z $RUN_DIR ] || [ -z $FASTQ_DIR ]; then
-    echo "Variables not supplied correctly. Use -h/--help options for assistance. Exiting with code 1"
-    exit 1
-fi
-if [ -z $SAMPLE_SHEET ]; then
-    SAMPLE_SHEET="${RUN_DIR}/SampleSheet.csv"
-fi
-if [ ! -f $SAMPLE_SHEET ]; then
-    echo "Sample sheet $SAMPLE_SHEET not found. Exiting with code 1"
-    exit 1
-fi
+echo -e "START: $(date)\nWGS WES Pipeline\nRun dir: $RUN_DIR\nSample sheet: $SAMPLE_SHEET\nFastq dir: $FASTQ_DIR"
 
 ml biology bcl2fastq
 bcl2fastq --runfolder-dir $RUN_DIR --sample-sheet $SAMPLE_SHEET --output-dir $FASTQ_DIR
+
+echo -e "END: $(date)\nRuntime: $(($(date +%s)-$START_TIME)) seconds"
+if [ $(find $FASTQ_DIR -name "*.fastq.gz" | wc -l) -eq 0 ]; then
+    echo "No fastq.gz files found in fastq dir. Exiting with code 1" >> $PIPELINE_STATUS
+    exit 1
+fi
+find $FASTQ_DIR -name "*.fastq.gz" -exec mv {} ${FASTQ_DIR}/ \;
+BCL_SIZE=$(du -sh $RUN_DIR | cut -f 1)
+UND_SIZE=$(du -shc ${FASTQ_DIR}/Undetermined*.fastq.gz | tail -n 1 | cut -f 1)
+FASTQ_SIZE=$(ls ${FASTQ_DIR}/*.fastq.gz | grep -v "Undetermined\|extracted" | xargs du -shc | tail -n 1 | cut -f 1)
+echo -e "$BCL_SIZE run dir produced $UND_SIZE of undetermined fastq.gz and $FASTQ_SIZE of determined fastq.gz" >> $PIPELINE_STATUS
+echo "### Demultiplexing ### - END: $(date)" >> $PIPELINE_STATUS
