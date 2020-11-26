0eb12bb8529f9ba8d11f027f230179fb50120fb5
David Klein <kleinjdavid@gmail.com>
2020-11-26 10:09:49 -0800

    remove unnecessary paranthesis

--- scripts/parse_blast_results.py
+++ scripts/parse_blast_results.py
@@ -9,7 +9,7 @@ blast_results_prefix = sys.argv[2]
 parsed_blast_results_filename = sys.argv[3]
 pipeline_status_filename = sys.argv[4]
 blastn_jsons = glob.glob("{}*.json".format(blast_results_prefix))
-pipeline_status_output = "{} blast json result files found with {} prefix\n".format(len(blastn_jsons), blast_results_prefix))
+pipeline_status_output = "{} blast json result files found with {} prefix\n".format(len(blastn_jsons), blast_results_prefix)
 all_top_blast_hits_list_list = []
 
 # for each blast result file, consolidate the top results
