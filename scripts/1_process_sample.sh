040d1b247431a100df4ba15983307ea26fef75d6
David Klein <kleinjdavid@gmail.com>
2020-11-30 08:10:43 -0800

    add rhesus filter

--- scripts/1_process_sample.sh
+++ scripts/1_process_sample.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=1_process_sample
 #SBATCH --mem=64GB
 #SBATCH --cpus-per-task=4
-#SBATCH --time=3-00:00:00
+#SBATCH --time=2-00:00:00
 #SBATCH --partition=cgawad
 
 START_TIME=$(date +%s)
@@ -12,11 +12,12 @@ RESULTS_DIR=$2
 R1_SUFFIX=$3
 R2_SUFFIX=$4
 SKIP_TRIMMOMATIC=$5
-REF_FASTA=$6
-KRAKEN_DB_TYPES_ARRAY=( $(echo $7 | sed 's/-/ /g') )
-KRAKEN_DB_DIR_PREFIX=$8
-TOOLS_DIR=$9
-SAMPLE_ARRAY=( $(echo ${10} | sed 's/:/ /g') )
+REF_FASTA_ARRAY=( $(echo $6 | sed 's/:/ /g') )
+REF_NAME_ARRAY=( $(echo $7 | sed 's/:/ /g') )
+KRAKEN_DB_TYPE_ARRAY=( $(echo $8 | sed 's/-/ /g') )
+KRAKEN_DB_DIR_PREFIX=$9
+TOOLS_DIR=${10}
+SAMPLE_ARRAY=( $(echo ${11} | sed 's/:/ /g') )
 SAMPLE=${SAMPLE_ARRAY[$(( $SLURM_ARRAY_TASK_ID - 1 ))]}
 
 echo -e "START: $(date)\nBacteria Pipeline\nFastq dir: $FASTQ_DIR\nResults dir: $RESULTS_DIR\nSample: $SAMPLE"
@@ -54,44 +55,72 @@ echo -e "sample\tread_count" > ${SAMPLE}.read_counts.tsv
 echo -e "$SAMPLE\t$READ_COUNT" >> ${SAMPLE}.read_counts.tsv
 echo "### Counting fastq read counts ### - END: $(date)"
 
-echo "### Aligning sample to human ### - START: $(date)"
-bwa aln -t 4 $REF_FASTA $R1_FASTQ > ${SAMPLE}_R1.sai
-bwa aln -t 4 $REF_FASTA $R2_FASTQ > ${SAMPLE}_R2.sai
-bwa sampe -a 700 $REF_FASTA ${SAMPLE}_R1.sai ${SAMPLE}_R2.sai $R1_FASTQ $R2_FASTQ | \
-    samtools view -b - | samtools sort -o ${SAMPLE}_human_aligned.bam -
-samtools index ${SAMPLE}_human_aligned.bam
-echo "### Aligning sample to human ### - START: $(date)"
+PREV_R1_FASTQ=$R1_FASTQ
+PREV_R2_FASTQ=$R2_FASTQ
+for ((REF_INDEX = 0 ; REF_INDEX < ${#REF_FASTA_ARRAY[@]} ; REF_INDEX++)); do
+    REF_FASTA=${REF_FASTA_ARRAY[$REF_INDEX]}
+    REF_NAME=${REF_NAME_ARRAY[$REF_INDEX]}
+    
+    echo "### Aligning sample to $REF_NAME ### - START: $(date)"
+    echo -e "Ref fasta: $REF_FASTA\nR1 fastq: $PREV_R1_FASTQ\nR2 fastq: $PREV_R2_FASTQ"
+    bwa aln -t 4 $REF_FASTA $PREV_R1_FASTQ > ${SAMPLE}_${REF_NAME}_R1.sai
+    bwa aln -t 4 $REF_FASTA $PREV_R2_FASTQ > ${SAMPLE}_${REF_NAME}_R2.sai
+    bwa sampe -a 700 $REF_FASTA ${SAMPLE}_${REF_NAME}_R1.sai ${SAMPLE}_${REF_NAME}_R2.sai \
+        $PREV_R1_FASTQ $PREV_R2_FASTQ | samtools view -b - | samtools sort -o ${SAMPLE}_${REF_NAME}_aligned.bam -
+    samtools index ${SAMPLE}_${REF_NAME}_aligned.bam
+    echo "### Aligning sample to $REF_NAME ### - END: $(date)"
+    
+    echo "### Collecting $REF_NAME alignment metrics ### - START: $(date)"
+    gatk --java-options "-XX:+UseParallelGC -XX:ParallelGCThreads=4 -Xmx64g" CollectAlignmentSummaryMetrics \
+        -R $REF_FASTA -I ${SAMPLE}_${REF_NAME}_aligned.bam -O ${SAMPLE}_${REF_NAME}_alignment_metrics.tsv
+    echo "### Collecting $REF_NAME alignment metrics ### - END: $(date)"
+    
+    echo "### Getting meta data for $REF_NAME matches from BAM ### - START: $(date)"
+    samtools view  ${SAMPLE}_${REF_NAME}_aligned.bam $(samtools view -H ${SAMPLE}_${REF_NAME}_aligned.bam | grep -P "@SQ.+SN:chr" | \
+        cut -f 2 | sed 's/^SN://')| cut -f 1 | sort -u > ${SAMPLE}_any_mapping_to_${REF_NAME}_query_names.txt
+    # samtools view ${SAMPLE}_${REF_NAME}_aligned.bam $(samtools view -H ${SAMPLE}_${REF_NAME}_aligned.bam | grep -P "@SQ.+SN:chr..?\t" | \
+    #     cut -f 2 | sed 's/^SN://') | awk '$7=="="' | cut -f 1 | sort -u > ${SAMPLE}_both_mapping_to_${REF_NAME}_query_names.txt
+    echo "### Getting meta data for $REF_NAME matches from BAM ### - END: $(date)"
 
-echo "### Collecting human alignment metrics ### - START: $(date)"
-gatk --java-options "-XX:+UseParallelGC -XX:ParallelGCThreads=4 -Xmx64g" CollectAlignmentSummaryMetrics \
-    -R $REF_FASTA -I ${SAMPLE}_human_aligned.bam -O ${SAMPLE}_human_alignment_metrics.tsv
-echo "### Collecting human alignment metrics ### - END: $(date)"
+    echo "### Filtering BAM for reads that don't match $REF_NAME ### - START: $(date)"
+    gatk --java-options "-XX:+UseParallelGC -XX:ParallelGCThreads=4 -Xmx64g" FilterSamReads \
+        --FILTER excludeReadList -I ${SAMPLE}_${REF_NAME}_aligned.bam -O ${SAMPLE}_no_${REF_NAME}.bam \
+        -RLF ${SAMPLE}_any_mapping_to_${REF_NAME}_query_names.txt --VALIDATION_STRINGENCY SILENT
+    gatk --java-options "-XX:+UseParallelGC -XX:ParallelGCThreads=4 -Xmx64g" SamToFastq -I ${SAMPLE}_no_${REF_NAME}.bam \
+        -F ${SAMPLE}_no_${REF_NAME}${R1_SUFFIX} -F2 ${SAMPLE}_no_${REF_NAME}${R2_SUFFIX} --VALIDATION_STRINGENCY SILENT
+    echo "### Filtering BAM for reads that don't match $REF_NAME ### - END: $(date)"
+    
+    PREV_R1_FASTQ=${SAMPLE}_no_${REF_NAME}${R1_SUFFIX}
+    PREV_R2_FASTQ=${SAMPLE}_no_${REF_NAME}${R2_SUFFIX}
+    rm ${SAMPLE}_${REF_NAME}_R1.sai ${SAMPLE}_${REF_NAME}_R2.sai
+    rm ${SAMPLE}_any_mapping_to_${REF_NAME}_query_names.txt
+    # rm ${SAMPLE}_no_${REF_NAME}.bam ${SAMPLE}_no_${REF_NAME}.bam.bai
+done
 
-echo "### Getting meta data for human matches from BAM ### - START: $(date)"
-samtools view  ${SAMPLE}_human_aligned.bam $(samtools view -H ${SAMPLE}_human_aligned.bam | grep -P "@SQ.+SN:chr" | \
-    cut -f 2 | sed 's/^SN://')| cut -f 1 | sort -u > ${SAMPLE}_any_mapping_to_human_query_names.txt
-# samtools view ${SAMPLE}_human_aligned.bam $(samtools view -H ${SAMPLE}_human_aligned.bam | grep -P "@SQ.+SN:chr..?\t" | \
-#     cut -f 2 | sed 's/^SN://') | awk '$7=="="' | cut -f 1 | sort -u > ${SAMPLE}_both_mapping_to_human_query_names.txt
-echo "### Getting meta data for human matches from BAM ### - END: $(date)"
-
-echo "### Filtering BAM for reads that don't match human ### - START: $(date)"
-gatk --java-options "-XX:+UseParallelGC -XX:ParallelGCThreads=4 -Xmx64g" FilterSamReads \
-    --FILTER excludeReadList -I ${SAMPLE}_human_aligned.bam -O ${SAMPLE}_no_human.bam \
-    -RLF ${SAMPLE}_any_mapping_to_human_query_names.txt --VALIDATION_STRINGENCY SILENT
-gatk --java-options "-XX:+UseParallelGC -XX:ParallelGCThreads=4 -Xmx64g" SamToFastq -I ${SAMPLE}_no_human.bam \
-    -F ${SAMPLE}_no_human${R1_SUFFIX} -F2 ${SAMPLE}_no_human${R2_SUFFIX} --VALIDATION_STRINGENCY SILENT
-echo "### Filtering BAM for reads that don't match human ### - END: $(date)"
+for ((REF_INDEX = 0 ; REF_INDEX < ${#REF_FASTA_ARRAY[@]} ; REF_INDEX++)); do
+    REF_FASTA=${REF_FASTA_ARRAY[$REF_INDEX]}
+    REF_NAME=${REF_NAME_ARRAY[$REF_INDEX]}
+    
+    NEXT_INDEX=$((REF_INDEX+1))
+    if [ $NEXT_INDEX -eq ${#REF_FASTA_ARRAY[@]} ]; then
+        mv ${SAMPLE}_no_${REF_NAME}${R1_SUFFIX} ${SAMPLE}_ref_filtered${R1_SUFFIX}
+        mv ${SAMPLE}_no_${REF_NAME}${R2_SUFFIX} ${SAMPLE}_ref_filtered${R2_SUFFIX}
+    else
+        echo "not removing filtered fastqs"
+        # rm ${SAMPLE}_no_${REF_NAME}${R1_SUFFIX} ${SAMPLE}_no_${REF_NAME}${R2_SUFFIX}
+    fi
+done
 
 echo "### De novo assembling contigs ### - START: $(date)"
 mkdir contigs_${SAMPLE}
 python3 /oak/stanford/groups/cgawad/Sequencing_Analysis_Tools/SPAdes-3.14.0-Linux/bin/spades.py \
-    -t 4 -m 64 -1 ${SAMPLE}_no_human${R1_SUFFIX} -2 ${SAMPLE}_no_human${R2_SUFFIX} -o contigs_${SAMPLE}
+    -t 4 -m 64 -1 ${SAMPLE}_ref_filtered${R1_SUFFIX} -2 ${SAMPLE}_ref_filtered${R2_SUFFIX} -o contigs_${SAMPLE}
 mv contigs_${SAMPLE}/contigs.fasta ${SAMPLE}_contigs.fasta
 echo "### De novo assembling contigs ### - END: $(date)"
 
 echo "### Aligning reads to contigs ### - START: $(date)"
 bwa index ${SAMPLE}_contigs.fasta
-bwa mem -M ${SAMPLE}_contigs.fasta ${SAMPLE}_no_human${R1_SUFFIX} ${SAMPLE}_no_human${R2_SUFFIX} | \
+bwa mem -M ${SAMPLE}_contigs.fasta ${SAMPLE}_ref_filtered${R1_SUFFIX} ${SAMPLE}_ref_filtered${R2_SUFFIX} | \
     samtools view -b - | samtools sort -o ${SAMPLE}_contig_aligned.bam -
 echo "### Aligning reads to contigs ### - END: $(date)"
 
@@ -101,11 +130,17 @@ gatk --java-options "-XX:+UseParallelGC -XX:ParallelGCThreads=4 -Xmx64g" Collect
 echo "### Collecting contig alignment metrics ### - END: $(date)"
 
 echo "### Exporting summarized and contig read targets from BAM ### - START: $(date)"
-HUMAN_ALIGNED_READS=$(samtools view ${SAMPLE}_human_aligned.bam | cut -f 3 | grep "chr" | wc -l)
+READ_TARGETS_HEADER="sample"
+READ_TARGETS_DATA="$SAMPLE"
+for REF_NAME in ${REF_NAME_ARRAY[@]}; do
+    TEMP_REF_ALIGNED_READS=$(samtools view ${SAMPLE}_${REF_NAME}_aligned.bam | cut -f 3 | grep "chr" | wc -l)
+    READ_TARGETS_HEADER+="\t${REF_NAME}_aligned"
+    READ_TARGETS_DATA+="\t${TEMP_REF_ALIGNED_READS}"
+done
 CONTIG_ALIGNED_READS=$(samtools view ${SAMPLE}_contig_aligned.bam | cut -f 3 | grep "NODE" | wc -l)
 UNALIGNED_READS=$(samtools view ${SAMPLE}_contig_aligned.bam | cut -f 3 | grep -v "NODE" | wc -l)
-echo -e "sample\thuman_aligned\tcontig_aligned\tunaligned" > ${SAMPLE}_summed_read_targets.tsv
-echo -e "${SAMPLE}\t${HUMAN_ALIGNED_READS}\t${CONTIG_ALIGNED_READS}\t${UNALIGNED_READS}" >> ${SAMPLE}_summed_read_targets.tsv
+echo -e "$READ_TARGETS_HEADER\tcontig_aligned\tunaligned" > ${SAMPLE}_summed_read_targets.tsv
+echo -e "$READ_TARGETS_DATA\t$CONTIG_ALIGNED_READS\t$UNALIGNED_READS" >> ${SAMPLE}_summed_read_targets.tsv
 
 echo -e "sample\ttarget" > ${SAMPLE}_contig_read_targets.tsv
 samtools view ${SAMPLE}_contig_aligned.bam | cut -f 3 | grep "NODE" > ${SAMPLE}_temp_contig_read_targets.txt
@@ -117,29 +152,31 @@ echo "### Exporting summarized and contig read targets from BAM ### - END: $(dat
 # ${TOOLS_DIR}/ncbi-blast-2.10.0+/bin/dustmasker -in ${SAMPLE}_contigs.fasta -outfmt fasta -out ${SAMPLE}_contigs_high_complexity.fasta
 # echo "### Marking low complexity regions in contigs ### - END: $(date)"
 
-echo "### Running kraken2 on non-human matches ### - START: $(date)"
-for DB_TYPE in ${KRAKEN_DB_TYPES_ARRAY[@]}; do
-    kraken2 --db ${KRAKEN_DB_DIR_PREFIX}${DB_TYPE} --threads 4 --output ${SAMPLE}_no_human_vs_kraken.tsv --paired --gzip-compressed \
-        --report ${SAMPLE}_${DB_TYPE}_kraken_report.tsv ${SAMPLE}_no_human${R1_SUFFIX} ${SAMPLE}_no_human${R2_SUFFIX}
+echo "### Running kraken2 on reference filtered matches ### - START: $(date)"
+for DB_TYPE in ${KRAKEN_DB_TYPE_ARRAY[@]}; do
+    kraken2 --db ${KRAKEN_DB_DIR_PREFIX}${DB_TYPE} --threads 4 --output ${SAMPLE}_ref_filtered_vs_kraken.tsv --paired --gzip-compressed \
+        --report ${SAMPLE}_${DB_TYPE}_kraken_report.tsv ${SAMPLE}_ref_filtered${R1_SUFFIX} ${SAMPLE}_ref_filtered${R2_SUFFIX}
 done
-echo "### Running kraken2 on non-human matches ### - END: $(date)"
+echo "### Running kraken2 on reference filtered matches ### - END: $(date)"
 
-if [ ! -f ${SAMPLE}_no_human_vs_kraken.tsv ]; then
-    echo "Final file ${SAMPLE}_no_human_vs_kraken.tsv not found. Exiting with code 1"
+if [ ! -f ${SAMPLE}_ref_filtered_vs_kraken.tsv ]; then
+    echo "Final file ${SAMPLE}_ref_filtered_vs_kraken.tsv not found. Exiting with code 1"
     exit 1
 fi
 if [ $SKIP_TRIMMOMATIC -eq 0 ]; then
     rm ${SAMPLE}_trimmomatic_log.txt
-    # rm $R1_FASTQ $R2_FASTQ
+    rm $R1_FASTQ $R2_FASTQ
     rm $UNPAIRED_R1_FASTQ $UNPAIRED_R2_FASTQ
 fi
+# for REF_NAME in ${REF_NAME_ARRAY[@]}; do
+#     rm ${SAMPLE}_${REF_NAME}_aligned.bam ${SAMPLE}_${REF_NAME}_aligned.bam.bai
+# done
 rm -r contigs_${SAMPLE} 
 rm ${SAMPLE}_contigs.fasta.amb ${SAMPLE}_contigs.fasta.ann ${SAMPLE}_contigs.fasta.bwt
 rm ${SAMPLE}_contigs.fasta.pac ${SAMPLE}_contigs.fasta.sa
-rm ${SAMPLE}_R1.sai ${SAMPLE}_R2.sai
-rm ${SAMPLE}_human_aligned.bam* ${SAMPLE}_no_human.bam* ${SAMPLE}_contig_aligned.bam*
+rm ${SAMPLE}_contig_aligned.bam*
 rm ${SAMPLE}_temp_contig_read_targets.txt
-rm ${SAMPLE}_no_human${R1_SUFFIX} ${SAMPLE}_no_human${R2_SUFFIX}
-rm ${SAMPLE}_any_mapping_to_human_query_names.txt
-rm ${SAMPLE}_no_human_vs_kraken.tsv
+# rm ${SAMPLE}_ref_filtered${R1_SUFFIX} ${SAMPLE}_ref_filtered${R2_SUFFIX}
+rm ${SAMPLE}_any_mapping_to_ref_filtered_query_names.txt
+rm ${SAMPLE}_ref_filtered_vs_kraken.tsv
 echo -e "END: $(date)\nRuntime: $(($(date +%s)-$START_TIME)) seconds"
--- submit_all.sh
+++ submit_all.sh
@@ -12,7 +12,7 @@ Purpose: \n\t\
     This pipeline is built to identify bacterial species from pair-end fastq.gz files and remove human contamination \n\n\
 Required arguments: -p/--project <arg> and either -f/--fastq_dir <arg> or -r/--results_dir <arg> \n\
 Optional arguments: -b/--run_dir <arg>, --sample_sheet <arg>, --R1_suffix <arg>, --R2_suffix <arg>, --err_out_dir <arg>, \n\t\
-    --skip_trimming, --skip_identify, --only_identify, --contig_len_min <arg>, --contig_align_min <arg>, --slurm <arg> \n\
+    --skip_trimming, --skip_identify, --filter_rhesus, --only_identify, --contig_len_min <arg>, --contig_align_min <arg>, --slurm <arg> \n\
 Defaults: \n\t\
     If no fastq_dir specified, uses results_dir \n\t\
     If no results_dir specified, makes new directory in fastq_dir \n\t\
@@ -31,6 +31,7 @@ For more information, read the README.md"
 
 # Reads in command line option arguments and assigns them to variables
 SKIP_TRIMMOMATIC=0
+FILTER_RHESUS=0
 SKIP_IDENTIFY=0
 ONLY_IDENTIFY=0
 CONTIG_LENGTH_MINIMUM=5000
@@ -71,6 +72,8 @@ while [ "$1" != "" ]; do
                                 ;;
         --skip_trimming )       SKIP_TRIMMOMATIC=1
                                 ;;
+        --filter_rhesus )       FILTER_RHESUS=1
+                                ;;
         --skip_identify )       SKIP_IDENTIFY=1
                                 ;;
         --only_identify )       ONLY_IDENTIFY=1
@@ -101,6 +104,7 @@ done
 REFERENCE_DIR="/oak/stanford/groups/cgawad/Reference_Files/GATK_Resource_Bundle_hg38"
 TOOLS_DIR="/oak/stanford/groups/cgawad/Sequencing_Analysis_Tools"
 REF_FASTA="${REFERENCE_DIR}/Homo_sapiens_assembly38.fasta"
+RHESUS_FASTA="/oak/stanford/groups/cgawad/Reference_Files/Macaca_mulattta_Rhesus_monkey_hg38/Macaca_mulatta_Rhesus_monkey_hg38.fasta"
 SCRIPT_DIR="${PIPELINE_DIR}/scripts"
 KRAKEN_DB_TYPES="microbial-plasmid-viral"
 KRAKEN_DB_DIR_PREFIX="/oak/stanford/groups/cgawad/Reference_Files/Kraken2_Fatfree_Databases/kraken2-fatfree-"
@@ -159,6 +163,9 @@ fi
 if [ $SKIP_TRIMMOMATIC -eq 1 ]; then
     OPTIONS+=( "--skip_trimming" )
 fi
+if [ $FILTER_RHESUS -eq 1 ]; then
+    OPTIONS+=( "--filter_rhesus" )
+fi
 if [ $SKIP_IDENTIFY -eq 1 ] && [ $ONLY_IDENTIFY -eq 1 ]; then
     echo "Variables not supplied correctly. Please specify either --skip_identify or --only_identify, not both. Exiting with code 1"
     exit 1
@@ -194,6 +201,9 @@ if [ "$TEMP_PIPELINE_DIR" = "$PIPELINE_DIR" ]; then
     # Optional variable definitions
     if [ $SKIP_TRIMMOMATIC -eq 1 ]; then
         echo "Skip trimming - will not run trimmomatic" >> $PIPELINE_STATUS
+    fi
+    if [ $FILTER_RHESUS -eq 1 ]; then
+        echo "Filter rhesus - will remove reads that align to macaca mulatta rhesus monkey" >> $PIPELINE_STATUS
     fi
     if [ $CONTIG_LENGTH_MINIMUM -eq 5000 ]; then
         echo "Contig length minimum: 5000 (default)" >> $PIPELINE_STATUS
@@ -258,6 +268,13 @@ if [ $STEP -eq 0 ]; then
 elif [ $STEP -eq 1 ]; then
     echo "### De novo assembling contigs and detecting contamination ### - START: $(date)" >> $PIPELINE_STATUS
     JOB_COUNT=${#SAMPLE_ARRAY[@]}
+    if [ $FILTER_RHESUS -eq 1 ]; then
+        REF_FASTA_STRING="${REF_FASTA}:${RHESUS_FASTA}"
+        REF_NAME_STRING="human:rhesus"
+    else
+        REF_FASTA_STRING="${REF_FASTA}"
+        REF_NAME_STRING="human"
+    fi
     echo "Process sample jobs to run: $JOB_COUNT" >> $PIPELINE_STATUS
     TEMP_ARRAY_INCREMENT=1000
     TEMP_ARRAY_START=1
@@ -268,7 +285,7 @@ elif [ $STEP -eq 1 ]; then
         TEMP_SAMPLES_STRING=$( IFS=$':'; echo "${TEMP_SAMPLE_ARRAY[*]}" )
         DEPENDENCIES+=( $(sbatch --parsable -e $STD_ERR_OUT_DIR/%A_%a_%x.err -o $STD_ERR_OUT_DIR/%A_%a_%x.out \
             --array=1-${TEMP_JOB_COUNT} ${SCRIPT_DIR}/1_process_sample.sh \
-            $FASTQ_DIR $RESULTS_DIR $R1_SUFFIX $R2_SUFFIX $SKIP_TRIMMOMATIC $REF_FASTA \
+            $FASTQ_DIR $RESULTS_DIR $R1_SUFFIX $R2_SUFFIX $SKIP_TRIMMOMATIC $REF_FASTA_STRING $REF_NAME_STRING \
             $KRAKEN_DB_TYPES $KRAKEN_DB_DIR_PREFIX $TOOLS_DIR $TEMP_SAMPLES_STRING) )
         TEMP_ARRAY_START=$(($TEMP_ARRAY_START + $TEMP_ARRAY_INCREMENT))
     done
@@ -378,7 +395,8 @@ elif [ $STEP -eq 3 ]; then
     
     
     echo "### Converting Kraken reports to TSV ### - START: $(date)" >> $PIPELINE_STATUS
-    for DB_TYPE in ${KRAKEN_DB_TYPES_ARRAY[@]}; do
+    KRAKEN_DB_TYPE_ARRAY=( $(echo $KRAKEN_DB_TYPES | sed 's/-/ /g') )
+    for DB_TYPE in ${KRAKEN_DB_TYPE_ARRAY[@]}; do
         SAMPLES_STRING=$( IFS=$':'; echo "${SAMPLE_ARRAY[*]}" )
         python3 ${SCRIPT_DIR}/kraken_report_to_jtree.py ${PROJECT}.${DB_TYPE}.kraken_reports.tsv \
             $IDENTIFY .${DB_TYPE}.kraken_jtree.json $SAMPLES_STRING
