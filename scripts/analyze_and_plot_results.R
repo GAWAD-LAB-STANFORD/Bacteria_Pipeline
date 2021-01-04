8ce30de84a83ae2c8297cbe661f8c117e72dcfae
David Klein <kleinjdavid@gmail.com>
2021-01-03 17:50:53 -0800

    plot species

--- scripts/analyze_and_plot_results.R
+++ scripts/analyze_and_plot_results.R
@@ -399,7 +399,7 @@ export_with_ncbi_annotations <- function(summed_df, ncbi_annotation_string, taxo
 
 
 # Nucleotide, plasmid, and viral BLAST results ------------------------------------------------
-taxonomic_variable_string_list <- c("Genus", "Plasmid", "Virus")
+taxonomic_variable_string_list <- c("Species", "Plasmid", "Virus")
 ncbi_annotation_string_list <- c("Microbe", "Plasmid", "Viral")
 
 # Load and preprocess all BLAST result dataframes
