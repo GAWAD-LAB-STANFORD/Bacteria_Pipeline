4304ffca6929f3c81224ab8622b27184753e13f4
David Klein <kleinjdavid@gmail.com>
2025-05-25 10:19:36 -0700

    error catching

--- scripts/parse_blast_results.py
+++ scripts/parse_blast_results.py
@@ -10,56 +10,61 @@ blast_results_prefix = sys.argv[2]
 parsed_blast_results_filename = sys.argv[3]
 pipeline_status_filename = sys.argv[4]
 blastn_jsons = glob.glob("{}*.json".format(blast_results_prefix))
-pipeline_status_output = "{} blast json result files found with {} prefix\n".format(len(blastn_jsons), blast_results_prefix)
 all_top_blast_hits_list_list = []
+json_files_with_errors = 0
+pipeline_status_output = ""
 
 
 # for each blast result file, consolidate the top results
 for blastn_json in blastn_jsons:
     json_file = open(blastn_json, "r")
-    blastn_dict = json.load(json_file)
-    json_file.close()
-    for blast_result in blastn_dict['BlastOutput2']:
-        extended_query_name = re.sub(" .+", "", blast_result['report']['results']['search']['query_title'])
-        sample = extended_query_name.split(".")[0]
-        query_name = ".".join(extended_query_name.split(".")[1:])
-        query_length = blast_result['report']['results']['search']['query_len']
-        hits = blast_result['report']['results']['search']['hits']
-        if len(hits) > 0:
-            for hit_count in range(len(hits)):
-                hit =  hits[hit_count]
-                fraction_identical = round(hit['hsps'][0]['identity'] / query_length, 4)
-                if db_type == "plasmid":
-                    before, keyword, after = hit['description'][0]['title'].partition("plasmid ")
-                    plasmid = after.split(" ")[0][:-1]
-                    source = " ".join(before.split(" ")[1:-1])
-                    new_addition = [sample, hit_count + 1, query_name, query_length,
-                                    hit['description'][0]['title'], hit['description'][0]['accession'],
-                                    plasmid, source, hit['hsps'][0]['align_len'], hit['len'],
-                                    hit['hsps'][0]['query_from'], hit['hsps'][0]['query_to'],
-                                    hit['hsps'][0]['hit_from'], hit['hsps'][0]['hit_to'],
-                                    hit['hsps'][0]['gaps'], hit['hsps'][0]['bit_score'], 
-                                    hit['hsps'][0]['evalue'], fraction_identical]
-                elif db_type == "viral":
-                    before, keyword, after = hit['description'][0]['title'].partition("phage ")
-                    phage = after.split(" ")[0][:-1]
-                    source = " ".join(before.split(" ")[1:-1])
-                    new_addition = [sample, hit_count + 1, query_name, query_length, 
-                                    hit['description'][0]['title'], hit['description'][0]['accession'],
-                                    phage, source, hit['hsps'][0]['align_len'], hit['len'],
-                                    hit['hsps'][0]['query_from'], hit['hsps'][0]['query_to'],
-                                    hit['hsps'][0]['hit_from'], hit['hsps'][0]['hit_to'],
-                                    hit['hsps'][0]['gaps'], hit['hsps'][0]['bit_score'], 
-                                    hit['hsps'][0]['evalue'], fraction_identical]
-                else:
-                    new_addition = [sample, hit_count + 1, query_name, query_length, 
-                                    hit['description'][0]['sciname'], hit['description'][0]['taxid'],
-                                    hit['description'][0]['accession'], hit['hsps'][0]['align_len'], hit['len'],
-                                    hit['hsps'][0]['query_from'], hit['hsps'][0]['query_to'],
-                                    hit['hsps'][0]['hit_from'], hit['hsps'][0]['hit_to'],
-                                    hit['hsps'][0]['gaps'], hit['hsps'][0]['bit_score'], 
-                                    hit['hsps'][0]['evalue'], fraction_identical]
-                all_top_blast_hits_list_list.append(new_addition)
+    try:
+        blastn_dict = json.load(json_file)
+        json_file.close()
+        for blast_result in blastn_dict['BlastOutput2']:
+            extended_query_name = re.sub(" .+", "", blast_result['report']['results']['search']['query_title'])
+            sample = extended_query_name.split(".")[0]
+            query_name = ".".join(extended_query_name.split(".")[1:])
+            query_length = blast_result['report']['results']['search']['query_len']
+            hits = blast_result['report']['results']['search']['hits']
+            if len(hits) > 0:
+                for hit_count in range(len(hits)):
+                    hit =  hits[hit_count]
+                    fraction_identical = round(hit['hsps'][0]['identity'] / query_length, 4)
+                    if db_type == "plasmid":
+                        before, keyword, after = hit['description'][0]['title'].partition("plasmid ")
+                        plasmid = after.split(" ")[0][:-1]
+                        source = " ".join(before.split(" ")[1:-1])
+                        new_addition = [sample, hit_count + 1, query_name, query_length,
+                                        hit['description'][0]['title'], hit['description'][0]['accession'],
+                                        plasmid, source, hit['hsps'][0]['align_len'], hit['len'],
+                                        hit['hsps'][0]['query_from'], hit['hsps'][0]['query_to'],
+                                        hit['hsps'][0]['hit_from'], hit['hsps'][0]['hit_to'],
+                                        hit['hsps'][0]['gaps'], hit['hsps'][0]['bit_score'], 
+                                        hit['hsps'][0]['evalue'], fraction_identical]
+                    elif db_type == "viral":
+                        before, keyword, after = hit['description'][0]['title'].partition("phage ")
+                        phage = after.split(" ")[0][:-1]
+                        source = " ".join(before.split(" ")[1:-1])
+                        new_addition = [sample, hit_count + 1, query_name, query_length, 
+                                        hit['description'][0]['title'], hit['description'][0]['accession'],
+                                        phage, source, hit['hsps'][0]['align_len'], hit['len'],
+                                        hit['hsps'][0]['query_from'], hit['hsps'][0]['query_to'],
+                                        hit['hsps'][0]['hit_from'], hit['hsps'][0]['hit_to'],
+                                        hit['hsps'][0]['gaps'], hit['hsps'][0]['bit_score'], 
+                                        hit['hsps'][0]['evalue'], fraction_identical]
+                    else:
+                        new_addition = [sample, hit_count + 1, query_name, query_length, 
+                                        hit['description'][0]['sciname'], hit['description'][0]['taxid'],
+                                        hit['description'][0]['accession'], hit['hsps'][0]['align_len'], hit['len'],
+                                        hit['hsps'][0]['query_from'], hit['hsps'][0]['query_to'],
+                                        hit['hsps'][0]['hit_from'], hit['hsps'][0]['hit_to'],
+                                        hit['hsps'][0]['gaps'], hit['hsps'][0]['bit_score'], 
+                                        hit['hsps'][0]['evalue'], fraction_identical]
+                    all_top_blast_hits_list_list.append(new_addition)
+    except (ValueError):
+        pipeline_status_output += "\tError reading json file {} - this file is most likely empty\n".format(blastn_json)
+        json_files_with_errors += 1
 
 
 if len(all_top_blast_hits_list_list) > 0:
@@ -81,6 +86,7 @@ if len(all_top_blast_hits_list_list) > 0:
     blast_results_df.to_csv(parsed_blast_results_filename, header = True, index = False, sep="\t")
 
 
+pipeline_status_output = "{} processed of {} blast json result files found with {} prefix\n{}".format((len(blastn_jsons)-json_files_with_errors), len(blastn_jsons), blast_results_prefix, pipeline_status_output)
 file1 = open(pipeline_status_filename, "a")
 file1.write(pipeline_status_output) 
 file1.close()
